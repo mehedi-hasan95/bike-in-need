a2d6129ef720d6f6f0925aee303ee5a047aa8730
Mehedi Titu <mh.me.mehedi@gmail.com>
2022-11-24 11:08:26 +0600

    Error page done

--- src/Components/Router/Router.jsx
+++ src/Components/Router/Router.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Error from "../Pages/Error/Error";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Login/Register";
@@ -11,7 +12,8 @@ const router = createBrowserRouter([
       children: [
         {path: '/', element: <Home></Home>},
         {path: 'login', element: <Login></Login>},
-        {path: 'register', element: <Register></Register>}
+        {path: 'register', element: <Register></Register>},
+        {path: '*', element: <Error></Error>}
       ]
     },
   ]);
