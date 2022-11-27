57f3f7790c8ca7b8497a705fdc2416d4b8c76b34
mehedititu <mh.me.mehedi@gmail.com>
2022-11-27 18:44:08 +0600

    Advertised items shows on the UI

--- src/Components/Pages/Home/Advertised/Advertised.jsx
+++ src/Components/Pages/Home/Advertised/Advertised.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useState } from 'react';
+import Lodding from '../../Common/Lodding/Lodding';
 import SingleAdvertised from './SingleAdvertised';
+import BookingModal from '../../Modal/BookingModal';
 
 const Advertised = () => {
+    const { data: advertised, isLoading, refetch } = useQuery({
+        queryKey: ['advertised'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/products/advertised');
+            const data = await res.json();
+            refetch()
+            return data;
+        }
+    })
+
+    const [treatment, setBookNow] = useState(null);
+
+    if (isLoading) {
+        return <Lodding></Lodding>
+    }
     return (
-        <div>
-            <SingleAdvertised></SingleAdvertised>
+        <div className='container mx-auto mt-16'>
+            {
+                advertised.length > 0 && <div>
+                    <h2 className='text-center text-rose-500 font-semibold text-2xl'>This is Advertised Product</h2>
+                    <div className="gird grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                        {
+                            advertised.map(ad => <SingleAdvertised setBookNow={setBookNow} key={ad._id} ad={ad}></SingleAdvertised>)
+                        }
+                    </div>
+                    {
+                        treatment && <BookingModal treatment={treatment} setBookNow={setBookNow}></BookingModal>
+                    }
+                </div>
+            }
         </div>
     );
 };
--- src/Components/Pages/Home/Advertised/SingleAdvertised.jsx
+++ src/Components/Pages/Home/Advertised/SingleAdvertised.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const SingleAdvertised = () => {
+const SingleAdvertised = ({ ad, setBookNow }) => {
+    const {img, sale, title} = ad;
+    console.log(ad);
     return (
-        <div>
-            
+        <div className="card w-96 bg-base-100 shadow-xl mt-10">
+            <figure><img src={img} alt="Shoes" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{title}</h2>
+                <h3 className='text-xl'><span className='font-semibold'>Sold Price:</span> {sale}</h3>
+                <div className="card-actions justify-end">
+                <label onClick={() => setBookNow(ad)} htmlFor="open-modal" className="btn btn-primary">Book Now</label>
+                </div>
+            </div>
         </div>
     );
 };
